bf65a5c831b7983d5738a705b9af09798560e459
ralsen <follrichs@gmx.de>
2023-02-26 17:31:19 +0100

    changed " to '

--- progs/config.py
+++ progs/config.py
@@ -9,49 +9,49 @@ import socket
 import datetime
 
 def init():
-    global ymlcfg
+    global yml
     global DataPath, RRDPath, dataSuffix, hirestime
 
-    with open("../yml/config.yml", "r") as ymlfile:
-        ymlcfg = yaml.safe_load(ymlfile)
-    logSuffix = ymlcfg["suffixes"]["log"]
-    dataSuffix = ymlcfg["suffixes"]["data"]
-    logYML = ymlcfg["debug"]["logYML"]
-    debugdatefmt = ymlcfg["debug"]["datefmt"]
-    hirestime = ymlcfg["debug"]["hirestime"]
+    with open('../yml/config.yml', 'r') as ymlfile:
+        yml = yaml.safe_load(ymlfile)
+    logSuffix = yml['suffixes']['log']
+    dataSuffix = yml['suffixes']['data']
+    logYML = yml['debug']['logYML']
+    debugdatefmt = yml['debug']['datefmt']
+    hirestime = yml['debug']['hirestime']
 
-    LogPath = ymlcfg["pathes"]["ROOT_PATH"] + ymlcfg["pathes"]["LOG"]
-    DataPath = ymlcfg["pathes"]["ROOT_PATH"] + ymlcfg["pathes"]["DATA"]
-    RRDPath = ymlcfg["pathes"]["ROOT_PATH"] + ymlcfg["pathes"]["RRD"]
-    YMLPath = ymlcfg["pathes"]["ROOT_PATH"] + ymlcfg["pathes"]["YML"]
+    LogPath = yml['pathes']['ROOT_PATH'] + yml['pathes']['LOG']
+    DataPath = yml['pathes']['ROOT_PATH'] + yml['pathes']['DATA']
+    RRDPath = yml['pathes']['ROOT_PATH'] + yml['pathes']['RRD']
+    YMLPath = yml['pathes']['ROOT_PATH'] + yml['pathes']['YML']
 
     x = datetime.datetime.now()
-    logging.basicConfig(filename=LogPath + socket.gethostname()+x.strftime(logSuffix)+".log",
+    logging.basicConfig(filename=LogPath + socket.gethostname()+x.strftime(logSuffix)+'.log',
                         level=logging.DEBUG,
                         format='%(asctime)s :: %(levelname)-s :: %(message)s [%(name)s] [%(lineno)s]',
                         datefmt=debugdatefmt)
 
-    logging.info("\r\n")
-    logging.info("-----------------------------------------------------------")
-    with open(YMLPath + ymlcfg["files"]["DATASTORE_YML"], "r") as file:
+    logging.info('\r\n')
+    logging.info('-----------------------------------------------------------')
+    with open(YMLPath + yml['files']['DATASTORE_YML'], 'r') as file:
         StoreYML = yaml.safe_load(file)
     
     Dstore = ds.DS(StoreYML) ######
-    # """  how to append a new DataStore from existing definition in datastore*.yml and set some values
-    Dstore.append("Gsund", "without_RRD_and_CONF")
+    # '''  how to append a new DataStore from existing definition in datastore*.yml and set some values
+    Dstore.append('Gsund', 'without_RRD_and_CONF')
     data = {}
-    data["Gsund"] = {}
-    data["Gsund"]["name"] = "mein"
-    data["Gsund"]["IP"] = "Name"
-    data["Gsund"]["Type"] = "tut"
-    data["Gsund"]["Version"] = "nix zur Sache"
+    data['Gsund'] = {}
+    data['Gsund']['name'] = 'mein'
+    data['Gsund']['IP'] = 'Name'
+    data['Gsund']['Type'] = 'tut'
+    data['Gsund']['Version'] = 'nix zur Sache'
     ds.handle_DataSet(data)
     
     #ds.DS(StoreYML) 
 
-    print(ymlcfg["devices"])
-    if "No_Name_70_03_9F_9A_7C_05" in ymlcfg["devices"]:
-        print("######### das gibt es ########")
+    print(yml['devices'])
+    if 'No_Name_70_03_9F_9A_7C_05' in yml['devices']:
+        print('######### das gibt es ########')
         
-    print(ymlcfg["archive"])
-    logging.info("everything initialized !!!")
+    print(yml['archive'])
+    logging.info('everything initialized !!!')
